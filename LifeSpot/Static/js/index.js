c516dff99959884ceb55472a85e732c55b9b41dd
sTorov <sotorov2011@yandex.ru>
2022-12-12 04:59:15 +0300

    Task 29.4.4

--- LifeSpot/Static/js/index.js
+++ LifeSpot/Static/js/index.js
@@ -1,34 +1,35 @@
-﻿const handleSession = function() {
+﻿function handleSession() {
     let session = new Map();
-    session.set("userAgent", window.navigator.userAgent);
 
+    session.set("userAgent", window.navigator.userAgent);
     session.set("userAge", prompt("Пожалуйста, введите ваш возраст"));
+    session.set("date", new Date().toLocaleString());
 
     if (session.get("userAge") < 18) {
-        alert("Извините, содержимое сайта только для лиц от 18 лет!");
+        alert("Извините, содержимое сайта только для лиц от 18 лет! Вы будете перенаправлены!");
         window.location.href = "https://www.google.com/";
     } else {
-        let date = new Date().toLocaleString();
-        alert(`Добро пожаловать на LifeSpot!\nТекущее время: ${date}`);
-        session.set("date", date);
+        alert(`Добро пожаловать на LifeSpot!\nТекущее время: ${session.get("date")}`);
     }
 
+    return session;
+}
+
+function printSession(session) {    
     for (let item of session) {
         console.log(item);
     }
 }
 
 
-const filter = function(str) {
-    str = str.toLowerCase();
-    let translations = document.getElementsByClassName('video-container');
-
+function filter(handlerInputStr) {
     //let input = document.getElementsByClassName('input')[0].value;
+    let translations = document.getElementsByClassName('video-container');
 
     for (let i = 0; i < translations.length; i++) {
         let title = translations[i].querySelector('h3').innerText.toLowerCase();
 
-        if (!title.includes(str)) {     //(!title.includes(input))
+        if (!title.includes(handlerInputStr())) {     //(!title.includes(input))
             translations[i].style.display = 'none';
         } else {
             translations[i].style.display = 'inline-block';
