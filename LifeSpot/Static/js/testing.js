1323461797d0edd9998d12b56f86fcd69fd2228f
sTorov <sotorov2011@yandex.ru>
2022-12-21 04:45:48 +0300

    Task 30.3.2

--- LifeSpot/Static/js/testing.js
+++ LifeSpot/Static/js/testing.js
@@ -2,6 +2,16 @@
 //let name = prompt("Введите ваше имя");
 //alert(`Добро пожаловать ${name}, символов в вашем имени: ${name.length}`); 
 
+//let element = document.getElementsByTagName("*");       //получение списка всех элементов на html-странице по тегу "*"
+//alert(`Общее количество элементов на странице: ${element.length}`)
 
-let element = document.getElementsByTagName("*");       //получение списка всех элементов на html-странице по тегу "*"
-alert(`Общее количество элементов на странице: ${element.length}`)
+function saveInputText() {
+    let element = document.querySelector('.input');
+    let newInput = element.value;
+
+    alert(`Предыдущее значение:\n${this.oldInput === '' || this.oldInput === undefined ? '<Пусто>' : this.oldInput}\n` +
+        `Новое значение:\n${newInput === '' ? '<Пусто>' : newInput}`);
+
+    this.oldInput = newInput;
+    element.value = '';
+}
